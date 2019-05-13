128f83a77c6e56074d7ea2647f53fc545bf11ab2
Codesworth <codezworth@gmail.com>
2019-05-13 19:44:13 +0000

    Initial Commit

---
+++ Elysium/Elysium.h
@@ -0,0 +1,19 @@
+//
+//  Elysium.h
+//  Elysium
+//
+//  Created by Shadrach Mensah on 13/05/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Elysium.
+FOUNDATION_EXPORT double ElysiumVersionNumber;
+
+//! Project version string for Elysium.
+FOUNDATION_EXPORT const unsigned char ElysiumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elysium/PublicHeader.h>
+
+
